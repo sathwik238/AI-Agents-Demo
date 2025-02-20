1603ae3ef0d021e070eb1527ac043366e1567581
sathwik-bollepalli <sathwik238@gmail.com>
2025-02-19 21:58:32 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool
+import datetime
+import requests
+import pytz
+import yaml
+# from tools.final_answer import FinalAnswerTool
+
+# from Gradio_UI import GradioUI
+
+print("Loading tools...")
